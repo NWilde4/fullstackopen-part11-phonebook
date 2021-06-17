7b03570feb7d934f2d8cec89f0550fa54157b115
NWilde4 <wilde.norman@yahoo.com>
2021-06-17 14:36:39 +0200

    add cypress plugin to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     "es6": true,
     "jest/globals": true,
     "node": true,
-    // "cypress/globals": true
+    "cypress/globals": true
   },
   "settings": {
     "react": {
@@ -23,7 +23,7 @@ module.exports = {
     "sourceType": "module"
   },
   "plugins": [
-    "react", "jest"
+    "react", "jest", "cypress"
   ],
   "rules": {
     "indent": [
